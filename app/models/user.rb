c10c1298e3736cf3f85b9c0a3984bb12899ccc25
dbc gogo <brayden.cleary@gmail.com>
2013-03-31 14:14:33 -0700

    undid

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,6 +19,7 @@ class User < ActiveRecord::Base
             :uniqueness => { :case_sensitive => false}
   validates :password, presence: true, length: {:minimum => 6}
   validates :password_confirmation, presence: true
+  
   before_save {|user| user.email = email.downcase}
 
 end
