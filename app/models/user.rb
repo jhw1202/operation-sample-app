04dc0fec63d55da5d978ad4b5ce288be0fab73f6
dbc gogo <dbc@dbc.com>
2013-03-31 14:15:39 -0700

    reundo

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,7 +19,6 @@ class User < ActiveRecord::Base
             :uniqueness => { :case_sensitive => false}
   validates :password, presence: true, length: {:minimum => 6}
   validates :password_confirmation, presence: true
-  
   before_save {|user| user.email = email.downcase}
 
 end
