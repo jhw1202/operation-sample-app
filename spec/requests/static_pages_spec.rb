dbd1a3ce5e43380693e59d6b081089b803fe5860
Brayden Cleary <brayden.cleary@gmail.com>
2013-03-30 17:53:56 -0700

    test for modified/refactored page titles

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,17 +3,25 @@ require 'spec_helper'
 describe "StaticPages" do
 
   let(:base_title) { "Ruby on Rails Tutorial Sample App"}
+  
   describe "Home Page" do
 
     it "should have the contents 'Sample App' " do
       visit '/static_pages/home'
       page.should have_content('Sample App')
     end
-    it "should have the right title Home" do
+    
+    it "should have the base title" do
       visit '/static_pages/home'
-      page.should have_selector('title', 
-                :text => "#{base_title} | Home")
+      page.should have_selector('title',
+                        :text => "Ruby on Rails Tutorial Sample App")
     end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Home')
+    end
+  
   end
 
   describe "Help page" do
